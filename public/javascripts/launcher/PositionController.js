9ead09b9a74710389205152baa64b1fdb5d24922
Paul Dijou <pdi@zenexity.com>
2013-08-30 17:10:48 +0200

    More awesomeness

--- public/javascripts/launcher/PositionController.js
+++ public/javascripts/launcher/PositionController.js
@@ -2,6 +2,20 @@ app.controller("PositionCtrl", ["$scope", "$rootScope", function ($scope, $rootS
   $scope.position = {};
 
   $scope.fire = function () {
-    console.log("fire");
+    console.log("fire at", $scope.position);
+    _.forEach($rootScope.selectedLaunchers(), function (launcher) {
+      launcher.fireAt($scope.position.x, $scope.position.y);
+    });
+
+    if (!$scope.isDigesting()) {
+      $scope.$digest();
+    }
   };
+
+  keypress.register_combo({
+    "keys": "space",
+    "on_keydown": function () {
+      $scope.fire();
+    }
+  });
 }]);
